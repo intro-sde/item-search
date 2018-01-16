21a29bfedf72a1f28cf457564a03e57b4c2b6a62
Unknown <julia.hermann93@gmail.com>
2018-01-16 18:11:25 +0100

    Extending search
     to multiple space seperated keyword search e.g. search?keyword=Toscana Cycling

--- src/rest/search/SearchItems.java
+++ src/rest/search/SearchItems.java
@@ -43,21 +43,37 @@ public class SearchItems {
 				 "https://sde-storage-ws.herokuapp.com/").build();
 	 }
 	
-	public static String search(String keyword) throws IOException {
+	public static String search(String keyword) throws IOException {	
 		WebTarget service = config();
-		Response resp = service.path("/rdb/items").queryParam("filter", SearchItems.conertKWToFilter(keyword)).request().accept(MediaType.APPLICATION_JSON).header("Content-type","application/json").get();
+		Response resp = service.path("/rdb/items").queryParam("filter", SearchItems.conertQueryToFilter(keyword)).request().accept(MediaType.APPLICATION_JSON).header("Content-type","application/json").get();
 		String response = resp.readEntity(String.class);
 		String json = format(response);
 		return json;
 		 
 	}
 
+	private static String conertQueryToFilter(String query) {
+		String[] parts = query.split(" ");
+		
+		String keyword = parts[0];
+		String filter = conertKWToFilter(keyword);
+		if(parts.length>1) {
+			for (int i=1;i<parts.length;i++) {
+				String nextKeyWord = parts[i];
+				filter = "(" + filter + ")" + " and " + "(" + conertKWToFilter(nextKeyWord) + ")";
+			}
+		}
+		System.out.println(filter);
+		return filter;
+		
+	}
+	
 	private static String conertKWToFilter(String keyword) {
 		keyword = "\""+keyword+"\"";
 		String filter = keyword + " in 'name' or " + keyword + " in 'city' or "+ keyword +" in 'type' or " +keyword + " in 'topic' or " +keyword+ " in 'from' or "+keyword+" in 'to' or " +keyword+" in 'address' or " + keyword+" in 'rating'";
-		System.out.println(filter);
 		return filter;
 	}
+	
 	
 	
 	
